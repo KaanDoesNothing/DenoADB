e24648feb6fede75f30c8a4892e7c449ed85a2df
kaan yucesoy <kaanyt123@gmail.com>
2020-11-16 10:10:24 +0100

    Combined the utils file into index.ts

--- index.ts
+++ index.ts
@@ -1,12 +1,33 @@
-import {cmd, shell, isTrue} from "./utils.ts";
-
 class ADB {
+    async cmd(code: any) {
+        const execute = Deno.run({
+            cmd: code.split(" "), 
+            stdout: "piped",
+            stderr: "piped"
+        });
+        
+        const output = await execute.output();
+        const finalOutput = new TextDecoder().decode(output);
+    
+        execute.close();
+    
+        return finalOutput;
+    }
+
+    async shell(input: string) {
+        return await this.cmd("adb shell " + input);
+    }
+
+    isTrue(val: string) {
+        return parseInt(val) === 1;
+    }
+
     async waitForConnection() {
-        return await cmd("adb wait-for-devices");
+        return await this.cmd("adb wait-for-devices");
     }
 
     async getScreenSize() {
-        const raw: string = await shell("wm size");
+        const raw: string = await this.shell("wm size");
 
         const filtered: string = raw.replace("Physical size: ", "");
 
@@ -19,31 +40,31 @@ class ADB {
     }
 
     async getScreenshot(dir: string) {
-        await cmd("adb shell screencap -p /sdcard/screen.png");
-        await cmd("adb pull /sdcard/screen.png " + dir);
-        await cmd("adb shell rm /sdcard/screen.png");
+        await this.cmd("adb shell screencap -p /sdcard/screen.png");
+        await this.cmd("adb pull /sdcard/screen.png " + dir);
+        await this.cmd("adb shell rm /sdcard/screen.png");
 
         return `${dir}/screen.png`;
     }
 
     async getSetting(namespace: string, setting: string) {
-        return await shell(`settings get ${namespace} ${setting}`);
+        return await this.shell(`settings get ${namespace} ${setting}`);
     }
 
     async getProp(prop: string) {
-        return await shell("getprop " + prop);
+        return await this.shell("getprop " + prop);
     }
 
     async getWifiEnabled() {
-        return isTrue(await this.getSetting("global", "wifi_on"));
+        return this.isTrue(await this.getSetting("global", "wifi_on"));
     }
 
     async getMobileDataEnabled() {
-        return isTrue(await this.getSetting("global", "mobile_data"));
+        return this.isTrue(await this.getSetting("global", "mobile_data"));
     }
     
     async getAirplaneEnabled() {
-        return isTrue(await this.getSetting("global", "airplane_mode_on"));
+        return this.isTrue(await this.getSetting("global", "airplane_mode_on"));
     }
 
     async getCarrier() {
@@ -51,27 +72,27 @@ class ADB {
     }
 
     async startApp(packageName: string) {
-        await shell(`monkey -p ${packageName} -c android.intent.category.LAUNCHER 1`);
+        await this.shell(`monkey -p ${packageName} -c android.intent.category.LAUNCHER 1`);
     }
     
     async click(x: number, y: number) {
-        await shell(`input tap ${x} ${y}`);
+        await this.shell(`input tap ${x} ${y}`);
     }
 
     async back() {
-        await shell(`input keyevent KEYCODE_BACK`);
+        await this.shell(`input keyevent KEYCODE_BACK`);
     }
 
     async home() {
-        await shell("input keyevent KEYCODE_HOME");
+        await this.shell("input keyevent KEYCODE_HOME");
     }
 
     async menu() {
-        await shell("input keyevent KEYCODE_MENU");
+        await this.shell("input keyevent KEYCODE_MENU");
     }
 
     async reboot() {
-        await cmd("adb reboot");
+        await this.cmd("adb reboot");
     }
 }
 
